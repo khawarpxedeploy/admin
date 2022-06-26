5b4a474f02cc8ff6cbf87a4dfff434b055a485f7
khawarpxedeploy <khawar@pxedeploy.com>
2022-06-26 18:27:04 +0500

    footer url updated

--- resources/views/admin/layouts/footers/nav.blade.php
+++ resources/views/admin/layouts/footers/nav.blade.php
@@ -1,7 +1,7 @@
 <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{ now()->year }} <a href="https://dtlogics.com" class="font-weight-bold ml-1" target="_blank">Einfach Himmlisch</a>
+            &copy; {{ now()->year }} <a href="{{ env('APP_URL') }}" class="font-weight-bold ml-1" target="_blank">Einfach Himmlisch</a>
         </div>
     </div>
 </div>
