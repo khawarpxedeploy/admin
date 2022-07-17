c97a480e06ea766bf93d59e9f76aaa22b578cdf4
Khawar <khawar@pxedeploy.com>
2022-07-17 17:29:01 +0500

    addons lisitng category wise

--- app/Http/Controllers/Api/Client/ProductsController.php
+++ app/Http/Controllers/Api/Client/ProductsController.php
@@ -11,56 +11,75 @@ use App\Http\Controllers\Controller;
 
 class ProductsController extends Controller
 {
-    public function productsList(Request $request){
+    public function productsList(Request $request)
+    {
 
         $products = Product::where('status', 1)
-        ->with('addons')
-        ->orderBy('id', 'desc')
-        ->paginate($request->limit ?? 5);
+            ->with('addons')
+            ->orderBy('id', 'desc')
+            ->paginate($request->limit ?? 5);
         $success['products'] = $products;
-        if($products->isEmpty()){
-     
+        if ($products->isEmpty()) {
+
             return $this->sendResponse($success, 'No products found!');
         }
-        foreach($products as $product){
-            if($product->questions){
+        foreach ($products as $product) {
+            if ($product->questions) {
                 $questions = json_decode($product->questions);
-                if($questions){
+                if ($questions) {
                     $temp = array();
-                    foreach($questions as $key => $question){
+                    foreach ($questions as $key => $question) {
                         $found = Question::select('id', 'question')->where('id', $question)->first();
                         $temp[] = $found;
                     }
                     $product->questions = $temp;
                 }
             }
-            if($product->filters){
+            if ($product->filters) {
                 $filters = json_decode($product->filters);
-                if($filters){
+                if ($filters) {
                     $temp2 = array();
-                    foreach($filters as $filter){
+                    foreach ($filters as $filter) {
                         $found = Filter::select('id', 'title')->where('id', $filter)->first();
                         $temp2[] = $found;
                     }
                     $product->filters = $temp2;
                 }
             }
-            if($product->addons){
+            if ($product->addons) {
                 $addons = $product->addons;
-                if($addons){
+                if ($addons) {
                     $temp3 = array();
-                    foreach($addons as $addon){
-                        
+                    foreach ($addons as $addon) {
+
                         $found = Addon::select('id', 'type', 'title', 'price')->where('id', $addon->addon_id)->first();
-                        
+
                         $temp3[] = $found;
                     }
                     unset($product->addons);
-                    $product->addons = $temp3;
+                    $weight = array_filter($temp3, function ($item) {
+                        return $item["type"] === 'weight';
+                    });
+                    $size = array_filter($temp3, function ($item) {
+                        return $item["type"] === 'size';
+                    });
+                    $stone = array_filter($temp3, function ($item) {
+                        return $item["type"] === 'stone';
+                    });
+                    $engraving = array_filter($temp3, function ($item) {
+                        return $item["type"] === 'engraving';
+                    });
+
+                    $product->addons = [
+                        'weight' => array_values((array)$weight),
+                        'size' => array_values((array)$size),
+                        'stone' => array_values((array)$stone),
+                        'engraving' => array_values((array)$engraving)
+                    ];
                 }
             }
         }
-        $products->makeHidden(['status','created_at','updated_at']);
+        $products->makeHidden(['status', 'created_at', 'updated_at']);
         return $this->sendResponse($success ?? [], 'Products found!.');
     }
 }
