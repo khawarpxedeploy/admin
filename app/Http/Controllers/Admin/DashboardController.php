688ddf1f08ae33f229365a44a30606f53929e20a
Khawar <khawar@pxedeploy.com>
2022-08-09 19:22:07 +0500

    cards counter

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Order;
 use App\Models\Customer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index(){
         $customers = Customer::get()->count();
-        return view('admin.modules.dashboard.dashboard', compact('customers'));
+        $orders = Order::get()->count();
+        $pending = Order::where('status', 'pending')->get()->count();
+        $accepted = Order::where('status', 'accepted')->get()->count();
+        $cancelled = Order::where('status', 'cancelled')->get()->count();
+        return view('admin.modules.dashboard.dashboard', compact('customers', 'orders', 'pending', 'accepted', 'cancelled'));
     }
 }
--- resources/views/admin/layouts/headers/cards.blade.php
+++ resources/views/admin/layouts/headers/cards.blade.php
@@ -9,7 +9,7 @@
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">{{__('Total Orders')}}</h5>
-                                    <span class="h2 font-weight-bold mb-0">0</span>
+                                    <span class="h2 font-weight-bold mb-0">{{$orders ?? 0}}</span>
                                 </div>
                                 <div class="col-auto">
                                     <div class="icon icon-shape bg-purple text-white rounded-circle shadow">
@@ -17,10 +17,23 @@
                                     </div>
                                 </div>
                             </div>
-                            <p class="mt-3 mb-0 text-muted text-sm">
-                                <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 0.00%</span>
-                                <span class="text-nowrap">{{__('Since last month')}}</span>
-                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 ">
+                    <div class="card card-stats mb-4 mb-xl-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h5 class="card-title text-uppercase text-muted mb-0">{{__('Pending Orders')}}</h5>
+                                    <span class="h2 font-weight-bold mb-0">{{$pending ?? 0}}</span>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="icon icon-shape bg-purple text-white rounded-circle shadow">
+                                        <i class="ni ni-shop"></i>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -30,7 +43,7 @@
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">{{__('Total Active Orders')}}</h5>
-                                    <span class="h2 font-weight-bold mb-0">0</span>
+                                    <span class="h2 font-weight-bold mb-0">{{$accepted ?? 0}}</span>
                                 </div>
                                 <div class="col-auto">
                                     <div class="icon icon-shape bg-green text-white rounded-circle shadow">
@@ -38,20 +51,16 @@
                                     </div>
                                 </div>
                             </div>
-                            <p class="mt-3 mb-0 text-muted text-sm">
-                                <span class="text-warning mr-2"><i class="fas fa-arrow-down"></i> 0.00%</span>
-                                <span class="text-nowrap">{{__('Since last week')}}</span>
-                            </p>
                         </div>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-6 ">
-                    <div class="card card-stats mb-4 mb-xl-0">
+                    <div class="card card-stats mb-4 mb-xl-0 mt-4">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">{{__('Total Delivered Orders')}}</h5>
-                                    <span class="h2 font-weight-bold mb-0">0</span>
+                                    <span class="h2 font-weight-bold mb-0">{{$delivered ?? 0}}</span>
                                 </div>
                                 <div class="col-auto">
                                     <div class="icon icon-shape bg-gray text-white rounded-circle shadow">
@@ -59,10 +68,6 @@
                                     </div>
                                 </div>
                             </div>
-                            <p class="mt-3 mb-0 text-muted text-sm">
-                                <span class="text-warning mr-2"><i class="fas fa-arrow-down"></i> 0.00%</span>
-                                <span class="text-nowrap">{{__('Since last week')}}</span>
-                            </p>
                         </div>
                     </div>
                 </div>
@@ -72,7 +77,7 @@
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">{{__('Total Cancelled Orders')}}</h5>
-                                    <span class="h2 font-weight-bold mb-0">0</span>
+                                    <span class="h2 font-weight-bold mb-0">{{$cancelled ?? 0}}</span>
                                 </div>
                                 <div class="col-auto">
                                     <div class="icon icon-shape bg-red text-white rounded-circle shadow">
@@ -80,10 +85,6 @@
                                     </div>
                                 </div>
                             </div>
-                            <p class="mt-3 mb-0 text-muted text-sm">
-                                <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 0.00%</span>
-                                <span class="text-nowrap">{{__('Since yesterday')}}</span>
-                            </p>
                         </div>
                     </div>
                 </div>
@@ -101,10 +102,6 @@
                                     </div>
                                 </div>
                             </div>
-                            <p class="mt-3 mb-0 text-muted text-sm">
-                                <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 0%</span>
-                                <span class="text-nowrap">{{__('Since last month')}}</span>
-                            </p>
                         </div>
                     </div>
                 </div>
